ae62155d6a20dca7778b449f8d70863601d435dc
Jed Fox <git@twopointzero.us>
2019-12-16 12:42:57 -0500

    Add status item.

--- Input Sources/AppDelegate.swift
+++ Input Sources/AppDelegate.swift
@@ -11,7 +11,7 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-
+    let statusItem = NSStatusBar.system.statusItem(withLength:NSStatusItem.squareLength)
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
