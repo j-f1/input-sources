fbc0c28d1d163d4c9cfdf3352ee56a9929771fbc
Jed Fox <git@twopointzero.us>
2019-12-16 12:37:40 -0500

    Initial Commit

---
+++ Input Sources/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Input Sources
+//
+//  Created by Jed Fox on 12/16/19.
+//  Copyright © 2019 Jed Fox. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
