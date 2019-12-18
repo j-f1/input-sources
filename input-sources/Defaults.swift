7c55ce592f73f6f07396112ec8c898425e1f75b6
Jed Fox <git@twopointzero.us>
2019-12-17 23:40:15 -0500

    Don’t show in dock by default

--- input-sources/Defaults.swift
+++ input-sources/Defaults.swift
@@ -9,7 +9,7 @@
 import Defaults
 
 extension Defaults.Keys {
-    static let showInDock = Key<Bool>("showInDock", default: true)
+    static let showInDock = Key<Bool>("showInDock", default: false)
     static let showMenuBG = Key<Bool>("showMenuBG", default: true)
     static let clickToCycle = Key<Bool>("clickToCycle", default: false)
 //    static let shortcut = Key<String>("shortcut", default: nil)
