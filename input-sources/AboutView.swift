763ed793bd80800dd7b98ad9129fb6acefbc3abb
Jed Fox <git@twopointzero.us>
2019-12-25 22:49:09 -0500

    Add credits

--- input-sources/AboutView.swift
+++ input-sources/AboutView.swift
@@ -1,30 +1,63 @@
 import SwiftUI
 
 struct AboutView: View {
+    private static let creditsURL = Bundle.main.url(forResource: "Credits", withExtension: "html")!
+    private static let credits = NSAttributedString(
+        html: try! Data(contentsOf: creditsURL),
+        baseURL: creditsURL,
+        documentAttributes: nil
+    )!
     var body: some View {
         VStack(alignment: HorizontalAlignment.center, spacing: 16) {
             Image(decorative: "App Icon")
             Text(getString(for: "CFBundleName")).font(Font.headline)
             Text("Version \(getString(for: "CFBundleShortVersionString")) (\(getString(for: "CFBundleVersion")))")
             Text(getString(for: "NSHumanReadableCopyright"))
+            ScrollableAttributedText(content: AboutView.credits)
+                .frame(width: 350, height: 138)
             HStack {
                 Button(action: {
                     NSWorkspace.shared.open(URL(string: "https://j-f1.github.io/input-sources/privacy")!)
                 }) {
                     Text("Privacy Policy")
                 }
-                Button(action: { NSApp!.terminate(nil) }) {
+                Button(action: { NSApp.terminate(nil) }) {
                     Text("Quit")
                 }
             }
         }
-        .padding(.vertical, 30)
+        .padding(.vertical, 20)
         .padding(.horizontal, 16)
         .frame(minWidth: 500)
     }
 
     func getString(for key: String) -> String {
         Bundle.main.object(forInfoDictionaryKey: key) as! String
+    }
+}
+
+struct ScrollableAttributedText: NSViewRepresentable {
+    typealias NSViewType = NSScrollView
+
+    let content: NSAttributedString
+    init(content: NSAttributedString) {
+        self.content = content
+    }
+
+    func makeNSView(context: NSViewRepresentableContext<ScrollableAttributedText>) -> NSViewType {
+        let scrollView = NSTextView.scrollableTextView()
+        scrollView.borderType = .lineBorder
+
+        let textView = scrollView.documentView as! NSTextView
+        textView.drawsBackground = false
+        textView.isEditable = false
+        textView.isSelectable = true
+
+        return scrollView
+    }
+    
+    func updateNSView(_ scrollView: NSViewType, context: NSViewRepresentableContext<ScrollableAttributedText>) {
+        (scrollView.documentView as! NSTextView).textStorage?.setAttributedString(content)
     }
 }
 
