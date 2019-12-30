af9717e4c7504f1345a7e5980b300c5e29e84de6
Jed Fox <git@twopointzero.us>
2019-12-30 10:37:47 -0500

    Swiftify the remaining objc

--- input-sources/InputSource.swift
+++ input-sources/InputSource.swift
@@ -12,18 +12,35 @@ var allLayouts: [InputSource] {
 
 var enabledLayouts: [InputSource] { allLayouts.filter { $0.enabled } }
 var currentKeyboardLayout: InputSource {
-    allLayouts[allLayouts.firstIndex(of: InputSource(source: TISCopyCurrentKeyboardInputSource().takeUnretainedValue()))!]
+    allLayouts[allLayouts.firstIndex(of: InputSource(source: TISCopyCurrentKeyboardInputSource().takeRetainedValue()))!]
 }
 
 struct InputSource: Equatable {
     let source: TISInputSource
 
-    var localizedName: String { getStringProp(source, kTISPropertyLocalizedName) }
-    var id: ID { getStringProp(source, kTISPropertyInputSourceID) }
-    var enabled: Bool { getBoolProp(source, kTISPropertyInputSourceIsEnabled) }
+    var localizedName: String { self[string: kTISPropertyLocalizedName] }
+    var id: ID { self[string: kTISPropertyInputSourceID] }
+    var enabled: Bool { self[bool: kTISPropertyInputSourceIsEnabled] }
     
     func activate() {
         TISSelectInputSource(source)
+    }
+    
+    subscript(string key: CFString) -> String {
+        let value: CFString = getValue(for: key)
+        return value as String
+    }
+    subscript(bool key: CFString) -> Bool {
+        let value: CFNumber = getValue(for: key)
+        return (value as NSNumber).boolValue
+    }
+
+    // Thanks Martin R! https://stackoverflow.com/a/59522437/5244995
+    func getValue<T: AnyObject>(for key: CFString) -> T {
+        Unmanaged<T>
+            .fromOpaque(TISGetInputSourceProperty(source, key))
+            .takeUnretainedValue()
+            as T
     }
 }
 extension InputSource: Identifiable {
--- input-sources/Utils.h
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-#import <Carbon/Carbon.h>
-
-NSString *getStringProp(TISInputSourceRef src, CFStringRef prop);
-BOOL getBoolProp(TISInputSourceRef src, CFStringRef prop);
